79e3632c2de8e5db7e23fd14180dcd48e99bd5b6
georgi-rm <georgi.rm@gmail.com>
2021-12-03 22:27:20 +0200

    Problem 7 "Recursive Fibonacci" from Exercise: Recursion and Backtracking

---
+++ T01RecursionAndBacktracking/P07RecursiveFibonacci/src/P07RecursiveFibonacci.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P07RecursiveFibonacci {
+    public static Map<Long, Long> fibonacciCache = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long number = Long.parseLong(scanner.nextLine());
+
+        long result = getFibonacci(number);
+        System.out.println(result);
+    }
+
+    private static long getFibonacci(long number) {
+        if (number <= 1) {
+            return 1;
+        }
+
+        if (fibonacciCache.containsKey(number)) {
+            return fibonacciCache.get(number);
+        }
+
+        long result = getFibonacci(number - 1) + getFibonacci(number - 2);
+        fibonacciCache.put(number, result);
+        return result;
+    }
+}
