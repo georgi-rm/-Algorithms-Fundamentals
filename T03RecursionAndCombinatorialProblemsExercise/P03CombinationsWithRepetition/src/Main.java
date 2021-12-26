2c272ba1e03ac1f61fa069f2cf98d11de2efc700
georgi-rm <georgi.rm@gmail.com>
2021-12-26 10:55:33 +0200

    Problem 3 "Combinations with Repetition" from Exercise: Recursion and Combinatorial Problems

--- T03RecursionAndCombinatorialProblemsExercise/P03CombinationsWithRepetition/src/Main.java
+++ T03RecursionAndCombinatorialProblemsExercise/P03CombinationsWithRepetition/src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     private static int numberOfElements;
-    private static int[] combination;
+    private static Integer[] combination;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,17 +12,17 @@ public class Main {
         numberOfElements = Integer.parseInt(scanner.nextLine());
         int k = Integer.parseInt(scanner.nextLine());
 
-        combination = new int[k];
+        combination = new Integer[k];
 
         combination(0, 1);
     }
 
     private static void combination(int position, int startElement) {
         if (position >= combination.length) {
-            for (int i = 0; i < combination.length; i++) {
-                System.out.printf("%d ", combination[i]);
-            }
-            System.out.println();
+            String output = Arrays.stream(combination)
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(" "));
+            System.out.println(output);
             return;
         }
 
